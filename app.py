bdf9eba2eea91d60f520bd5fee4050c4ecb2b9ac
DaBloat <kurtrusselvillamor1206@gmail.com>
2025-06-13 21:09:47 +0800

    heavy update

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def dashboard():
-    return render_template('index.html')
+def homepage():
+    greet = {'AM':'Good Morning', 'PM':'Good Afternoon'}
+    stats = {
+        'greet': greet[datetime.now().strftime("%p")],
+        'user': 'Admin'
+    }
+    return render_template('index.html', stat=stats)
 
 if __name__ == '__main__':
     app.run(debug=True)
